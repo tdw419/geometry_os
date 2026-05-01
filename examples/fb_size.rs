1ae92400226681176c8b04315e3345db0d82d68e
Timothy Whittaker <tdw419@gmail.com>
2026-05-01 18:17:07 -0500

    Remove diagnostic examples used for debugging file_browser

--- examples/fb_size.rs
+++
@@ -1,6 +0,0 @@
-use geometry_os::assembler::assemble;
-fn main() {
-    let source = std::fs::read_to_string("programs/file_browser.asm").unwrap();
-    let asm = assemble(&source, 0).unwrap();
-    println!("Bytecode size: {} words (0x{:x})", asm.pixels.len(), asm.pixels.len());
-}
